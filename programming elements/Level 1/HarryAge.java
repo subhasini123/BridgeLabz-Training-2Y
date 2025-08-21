5b17a16234b3ee031d093a7b4dc96904d2d05a35
Subhasini <suhani484548@gmail.com>
2025-08-21 10:38:43 +0530

    [Suhani] added file

---
+++ programming elements/Level 1/HarryAge.java
@@ -0,0 +1,13 @@
+//package programming elements.Level 1;
+
+public class HarryAge {
+    public static void main(String[] args) {
+        int birthYear = 2000;
+        int currentYear = 2024;
+
+        int age = currentYear - birthYear;
+
+        System.out.println("Harry's age in " + currentYear + " is " + age);
+    }
+}
+
