6e8e168a163539d6f33a9033209e3e12709a9aea
Subhasini <suhani484548@gmail.com>
2025-08-26 23:56:50 +0530

    [Suhani] Added file

---
+++ Control Flow/Level 1/SmallestOfThree.java
@@ -0,0 +1,27 @@
+// File: SmallestOfThree.java
+
+import java.util.Scanner;
+
+public class SmallestOfThree {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Input
+        System.out.print("Enter the first number: ");
+        int number1 = scanner.nextInt();
+
+        System.out.print("Enter the second number: ");
+        int number2 = scanner.nextInt();
+
+        System.out.print("Enter the third number: ");
+        int number3 = scanner.nextInt();
+
+        // Check if first is the smallest
+        boolean isSmallest = (number1 < number2) && (number1 < number3);
+
+        // Output
+        System.out.println("Is the first number the smallest? " + isSmallest);
+
+        scanner.close();
+    }
+}
