37e3c1edf5fa39aa7eaecb9f37ef1991bd931b3a
Subhasini <suhani484548@gmail.com>
2025-08-27 21:24:09 +0530

    [Suhani] Added file

---
+++ Control Flow/Level 1/Level 2/FizzBuzz.java
@@ -0,0 +1,34 @@
+// File: FizzBuzz.java
+
+import java.util.Scanner;
+
+public class FizzBuzz {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Take user input
+        System.out.print("Enter a positive integer: ");
+        int number = scanner.nextInt();
+
+        // Check if positive integer
+        if (number <= 0) {
+            System.out.println("Invalid input! Please enter a positive integer.");
+        } else {
+            // Loop from 1 to number
+            for (int i = 1; i <= number; i++) {
+                if (i % 3 == 0 && i % 5 == 0) {
+                    System.out.println("FizzBuzz");
+                } else if (i % 3 == 0) {
+                    System.out.println("Fizz");
+                } else if (i % 5 == 0) {
+                    System.out.println("Buzz");
+                } else {
+                    System.out.println(i);
+                }
+            }
+        }
+
+        scanner.close();
+    }
+}
+
